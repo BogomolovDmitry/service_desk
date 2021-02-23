ae1902c107f5207054365ee8cb2e52c232aed428
BogomolovDmitry <31962802+BogomolovDmitry@users.noreply.github.com>
2021-02-23 16:26:41 +0300

    connect-to-pg

--- service_desk/settings.py
+++ service_desk/settings.py
@@ -54,8 +54,7 @@ ROOT_URLCONF = 'service_desk.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,13 +73,22 @@ WSGI_APPLICATION = 'service_desk.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'SD',
+            'USER': 'postgres',
+            'PASSWORD': 'WinD1997',
+            'HOST': '127.0.0.1',
+            'PORT': '5432'
+            }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
